89a6779b65481465deb87a3b7c7cae1e37c982f3
SpiderStan <107142794+SpiderStan@users.noreply.github.com>
2022-06-26 23:00:11 +0200

    Add files via upload

--- streamlit/pages/system_settings.py
+++ streamlit/pages/system_settings.py
@@ -1,7 +1,38 @@
 import streamlit as st
+import os
+import base64
+from PIL import Image
 import pandas as pd 
 from .singletons import settings
-    
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("background", "background.png"), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to background".format("background.png"))
+
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+    The bg will be static and won't take resolution of device into account.
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
 def app():
 
     with st.form("App Title Form", clear_on_submit=True):
@@ -21,6 +52,16 @@ def app():
         if submitted:
             st.session_state.ip_address = ip_address
             st.experimental_rerun()
+            
+    with st.form("App Background Form", clear_on_submit=True):
+        background_image_upload = st.file_uploader("App Background (PNG)", type=['png'], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+        submitted = st.form_submit_button(f"Change {st.session_state.create_emoji}")
+
+        if submitted:
+            if background_image_upload is not None:
+                save_uploadedfile(background_image_upload)
+                set_bg_hack('background/background.png')                
+                st.experimental_rerun()            
 
     if st.button(f"Back {st.session_state.back_emoji}"):
         st.session_state.nextpage = "pre_mainpage"
